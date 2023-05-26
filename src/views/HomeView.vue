3cad29009ed6e9310a133e92f1f84d77ae1fad34
Felicia Hoeft <fhoeft92615@gmail.com>
2023-05-26 18:24:52 -0500

    added router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="State Hero" src="../assets/home-hero/home-hero.jpg">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
