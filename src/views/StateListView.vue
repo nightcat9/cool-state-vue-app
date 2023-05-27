22703d703d4fc8b17bd24bc095d15397724cba51
Felicia Hoeft <fhoeft92615@gmail.com>
2023-05-27 15:27:16 -0500

    Load data on statelistview

--- src/views/StateListView.vue
+++ src/views/StateListView.vue
@@ -1,14 +1,29 @@
 <template>
-    <div class="home">
-        This is states table
-    </div>
+    <table>
+        <tr>
+            <th>State</th>
+            <th>Capital</th>
+            <th>State Bird</th>
+            <th>State Flower</th>
+        </tr>
+        <tr v-for="state in statesJson">
+            <td>{{ state.name }}</td>
+            <td>{{ state.capital }}</td>
+            <td>{{ state.bird }}</td>
+            <td>{{ state.flower }}</td>  
+        </tr>
+    </table>
 </template>
   
 <script>
+import json from '../../states.json'
 export default {
     name: 'StateListView',
-    components: {
-
+    components: {},
+    data() {
+        return {
+            statesJson: json
+        }
     }
 }
 </script>
