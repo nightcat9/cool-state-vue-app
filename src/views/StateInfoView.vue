a7fabc1e921c2b44c8045cbfd49f95d5c05d5b6e
Felicia Hoeft <fhoeft92615@gmail.com>
2023-05-27 20:02:32 -0500

    added all info to states.json

--- src/views/StateInfoView.vue
+++ src/views/StateInfoView.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         This is a state! - {{ $route.params.abbr }}
+        {{ thisState.name }}
     </div>
 </template>
   
 <script>
+import statesJson from '../../states.json'
 export default {
     name: 'StateInfoView',
-    components: {}
+    components: {},
+    data() {
+        return {
+            thisState : statesJson.find(state => state.abbreviation === this.$route.params.abbr)
+        }
+    }
 }
 </script>
